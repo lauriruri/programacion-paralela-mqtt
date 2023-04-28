673d041b5a857e5ea2d628c1e3028a6e255914c5
laura <lherre10@ucm.es>
2023-04-28 13:00:58 +0200

    separar enteros y reales y ver si es primo

--- 1ejercicio_numeros.py
+++ 1ejercicio_numeros.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from paho.mqtt.client import Client
+import math
 
 def on_message(mqttc, userdata, msg):
     d = float(msg.payload)
     n = int(round(d))
     print("Número",msg.payload,'int',n,'float',d, 'entero' if d==n else 'real')
+    print("¿Es primo ", n, "?",  'si' if es_primo(n) else 'no')
+    
+def es_primo(n):
+    for i in range(2,int(math.sqrt(n))+1):
+        if (n%i)==0:
+            return False
+    return True
 
 def main():
     mqttc = Client()
